01f53400fae0bbdd83df2414199ce8c7fa05e5e6
David Stinnett <davidstinnett@icloud.com>
2020-12-17 14:18:12 -0500

    heroku!!!!!

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,16 @@
 var path = require("path");
 
 module.exports = function(app) {
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
   // Called when "Countinue Workout" or "new Workout" is clicked in index.html
-  app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
+    app.get("/exercise", function(req, res) {
+     res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
   // Not quite sure what this is used for yet ....
-  app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
+    app.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
 };
